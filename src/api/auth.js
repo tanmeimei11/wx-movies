86b35536d6c06f7d29ef7202ffd087aae542cecc
xiangxiang <jiuyan@jiuyandeMacBook-Pro.local>
2018-02-02 15:04:30 +0800

    add  auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,19 +1,26 @@
 import wepy from 'wepy';
 import base from './base';
 import tips from '@/utils/tips';
+import event from '@/utils/event';
 
 export default class auth extends base {
-  static _readyStatus = false
+  static _readyStatus
 
   /**
    * 授权登录准备
    * 授权弹窗如果取消 PromiseStatus:pending
    */
   static async ready () {
-    return this._readyStatus ? Promise.resolve() : await this.login();
+    return !this._readyStatus ? this._register() : this._readyStatus;
   }
 
-  static _count = 0
+  static _register () {
+    // 注册准备完毕通知
+    this._readyStatus = new Promise( resolve => event.$on( 'ready', resolve ) );
+    // 授权流程
+    this.login();
+    return this._readyStatus;
+  }
 
   /**
    * 授权登录
@@ -23,7 +30,7 @@ export default class auth extends base {
     try {
       tips.setLoading();
       const { code } = await wepy.login();
-      const { encryptedData, iv } = await wepy.getUserInfo({ lang: 'zh_CN' })
+      const { encryptedData, iv } = await wepy.getUserInfo( { lang: 'zh_CN' } );
       // console.log( encryptedData )
       // const { qr } = wepy.$instance.globalData.qrcode_from
       const { tg_auth: token, _aries } = await this.post( `${this.baseUrl}/api/login`, { data: { code, encryptedData, iv, qrcode_from: wepy.$instance.globalData.qrcode_from } } );
@@ -31,15 +38,14 @@ export default class auth extends base {
       wepy.$instance.globalData.xAries = _aries;
       this._readyStatus = true;
       console.log( `code: ${code}\ntoken: ${token}` );
-    } catch (e) {
-      if (e.errMsg.indexOf('getUserInfo:fail') >= 0 && this._count === 0) {
-        this._count ++
-        const rst = await wepy.showModal({ title: '授权提示', content: '请开启“用户信息”权限', showCancel: true, cancelText: '拒绝', confirmText: '授权' })
-        this._count --
-        if (rst.confirm) wepy.openSetting()
+      event.$emit( 'ready' );
+    } catch ( e ) {
+      if ( e.errMsg.indexOf( 'getUserInfo:fail' ) >= 0 ) {
+        const rst = await wepy.showModal( { title: '授权提示', content: '请开启“用户信息”权限', showCancel: true, cancelText: '拒绝', confirmText: '授权' } );
+        if ( rst.confirm ) wepy.openSetting();
       }
 
-      throw new Error('未授权授权')
+      throw new Error( '未授权授权' );
     }
   }
 }
