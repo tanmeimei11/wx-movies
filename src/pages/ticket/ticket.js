1d2b778ab98ea6e8014fa1065b47fabfa081d6dd
小夫 <xiaofu@in66.com>
2018-02-03 15:17:33 +0800

    add

---
+++ src/pages/ticket/ticket.js
@@ -0,0 +1,112 @@
+import wepy from 'wepy';
+import auth from '@/api/auth';
+import Self from '@/api/self';
+import tips from '@/utils/tips';
+import { request } from '@/utils/request';
+import report from '@/components/report-submit';
+import track from '@/utils/track';
+
+export default class self extends wepy.page {
+  config = {
+    navigationBarTitleText: '我的电影卡'
+  }
+  components = { report }
+
+  data = {
+    num: '',
+    type: '',
+    btninfo: {},
+    isShowMobile: false,
+    isFull: false,
+    cards: [],
+    cardNum: 0,
+    rules: [], // 规则文案
+    userInfo: { // 用户信息
+      avatar: '',
+      name: '',
+      phone: ' '
+    },
+    cardInfos: [{ // 卡片信息
+      id: '',
+      title: 'in同城趴·电影王卡',
+      desc: '可任意次数兑换观影券',
+      time: '',
+      isApply: true,
+      num: ''
+    }]
+  }
+
+  methods = {
+    bindKeyInput ( e ) {
+      this.num = e.detail.value;
+      if ( e.detail.value.length === 11 ) {
+        this.isFull = true;
+      }
+    },
+    async submit () {
+      if ( this.isFull ) {
+        tips.loading();
+        var res = await request( {
+          url: '/mnp/user/update_phone',
+          method: 'POST',
+          data: {
+            phone: this.num
+          }
+        } );
+        if ( res.succ ) {
+          tips.loaded();
+          this.userInfo.phone = this.num;
+          await tips.success( this.type + '成功' );
+          this.isShowMobile = false;
+          this.$apply();
+        } else {
+          tips.loaded();
+          tips.error( '网络错误' );
+        }
+      }
+    },
+    open ( e ) {
+      console.log( e );
+      this.isShowMobile = true;
+      this.type = e.currentTarget.dataset.type;
+    },
+    close () {
+      this.isShowMobile = false;
+    },
+    toCard ( e ) {
+      this.cardNum = e.currentTarget.dataset.index;
+      track( 'my_card_click' );
+      wepy.navigateTo( {
+        url: `/pages/card/card?card_id=${e.currentTarget.dataset.id}`
+      } );
+    },
+    apply () {
+      if ( this.btninfo.cf_start === 'false' ) {
+        tips.error( this.btninfo.cf_start_desc );
+        return;
+      }
+      wepy.navigateTo( {
+        url: '/pages/detail/detail'
+      } );
+    }
+  }
+
+  async init () {
+    var myInfoRes = await Self.getMyInfo();
+    this.btninfo = myInfoRes;
+    this.cards = myInfoRes.cards;
+    console.log( myInfoRes.cards );
+    this.cardInfos = Self.initCardInfo( myInfoRes.cards );
+    this.userInfo = Self.initUserInfo( myInfoRes );
+    this.rules = Self.initRules( myInfoRes.texts );
+    this.$apply();
+  }
+  async onLoad () {
+    track( 'my_page_enter' );
+  }
+  async onShow () {
+    track( 'my_page_screen' );
+    await auth.ready();
+    await this.init();
+  }
+}
