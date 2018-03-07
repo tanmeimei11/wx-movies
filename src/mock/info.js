e74aa741761af3640c8b2fd15c2622bed720d059
小夫 <xiaofu@in66.com>
2018-03-08 01:46:57 +0800

    add

--- src/mock/info.js
+++ src/mock/info.js
@@ -6,6 +6,7 @@ module.exports = {
     'desc01': '109元三个月电影任你看',
     'cf_start_desc': '活动未开始~~',
     'bg_img': 'https://inimg07.jiuyan.info/in/2018/03/08/A25925A3-7D7C-E400-7E42-572B16F81F5A.jpg',
+    'btn_img': 'https://inimg01.jiuyan.info/in/2018/03/08/335EF61D-758E-EDA0-EF33-19370C156D72.png',
     'cf_start': 'true'
   },
   'code': '0',
