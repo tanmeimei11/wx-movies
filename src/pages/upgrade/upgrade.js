c0f492eeae74fc938362917036a33df504df56e4
小夫 <xiaofu@in66.com>
2018-03-09 11:35:41 +0800

    upgrade1

---
+++ src/pages/upgrade/upgrade.js
@@ -0,0 +1,34 @@
+import wepy from 'wepy';
+import auth from '@/api/auth';
+import Ticket from '@/api/ticket';
+import tips from '@/utils/tips';
+import report from '@/components/report-submit';
+import shareConnectMixin from '@/mixins/shareConnectMixin';
+import receiveFaildModal from '@/components/detail/receiveFaildModal';
+import adBanner from '@/components/adBanner';
+import track from '@/utils/track';
+
+export default class upgrade extends wepy.page {
+  config = {
+    navigationBarTitleText: ''
+  }
+  components = { report }
+  mixins = []
+  data = {
+  }
+
+  events = {
+  }
+
+  methods = {
+  }
+
+  async init () {
+    // var myInfoRes = await Ticket.getMyInfo();
+    this.$apply();
+  }
+  async onLoad ( options ) {
+    await auth.ready();
+    await this.init();
+  }
+}
