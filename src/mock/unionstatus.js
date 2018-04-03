776fb09afb7eafa90621c3e7d1c7620289c8e33f
xiangxiang <jiuyan@jiuyandeMacBook-Pro.local>
2018-04-03 14:57:25 +0800

    add union

--- src/mock/unionstatus.js
+++ src/mock/unionstatus.js
@@ -18,11 +18,17 @@ module.exports = {
     union_tips: '请在in同城趴app中查看已获得的电影票',
     union_need_count: 3,
     union_remain_time: 1000, // 倒计时剩余秒数
+    unionResult: {
+      isShow: false,
+      desc: '',
+      class: '',
+      tips: ''
+    },
     btns: [
       {
         btn_name: '去使用',
-        btn_color: '#FFFF',
-        btn_type: 0, // 0:小程序跳转路径   1:跨小程序跳转路径   2:转发按钮   3:下单支付
+        btn_color: 'btn-union',
+        btn_type: '0', // 0:小程序跳转路径   1:跨小程序跳转路径   2:转发按钮   3:下单支付
         btn_path: '/pages/detail/detail', // 跳转路径   用于0和1状态
         btn_app_id: 'xxxxxxx', // 跳转小程序appid     用于1状态
         btn_share_callback_url: 'http://xxxxxx/xxxx.jpg', // 分享成功之后的回调  用于2状态
