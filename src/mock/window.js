9f9520ca64d64af2bb4407addc857e1ad7a3f942
小夫 <xiaofu@in66.com>
2018-04-26 11:38:09 +0800

    add

--- src/mock/window.js
+++ src/mock/window.js
@@ -3,8 +3,9 @@ module.exports = {
     "succ": true,
     "data": {
         "img_url": "https://inimg05.jiuyan.info/in/2018/01/26/D79557A6-9FBD-C33A-2298-C262E84FA7CA.png",
-        "type": "path", //h5 打开h5页面、miniprogram 跳转其他小程序、path 跳转内部页面、custom 跳客服
+        "type": "custom", //h5 打开h5页面、miniprogram 跳转其他小程序、path 跳转内部页面、custom 跳客服
         "landing_path": "/pages/detail/detail?product_id=12",
+        'session_from': "xxx",
         "app_id": "wx5739539532",  // 用于miniprogram
     },
     "code": "0",
