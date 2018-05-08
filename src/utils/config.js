9905c6542da6b70265d97671a9630433833f9eba
小夫 <xiaofu@in66.com>
2018-05-08 22:49:33 +0800

    add

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,21 +1,21 @@
 
-var _station = '02'; // 上海
-// var _station = '2101'; // 杭州
+// var _station = '02'; // 上海
+var _station = '2101'; // 杭州
 module.exports = {
-  _v: '0.6.6',
+  _v: '0.6.7',
   isMock: false,
   isTrack: false,
   station: _station,
-  // DOMAIN: 'https://crowdfunding.in66.com',
-  DOMAIN: 'http://qacrowd-zf.in66.com',
+  DOMAIN: 'https://crowdfunding.in66.com',
+  // DOMAIN: 'http://qacrowd-zf.in66.com',
   trackId: _station === '02' ? 'UA-117482838-1' : 'UA-113017547-1',
   // DOMAIN: 'http://qacrowd-zf.in66.com',
   qnTokenUrl: 'https://www.in66.com/promo/commonapi/qiniutoken',
   // qnUploadUrl: "http://upload.qiniup.com/",
   qnUploadUrl: 'https://upload.qiniup.com/',
   qnResUrl: 'https://inimg07.jiuyan.info/',
-  payUrl: 'http://qainlove.in66.com/api/payment/signature',
-  // payUrl: 'https://love.in66.com/api/payment/signature',
+  // payUrl: 'http://qainlove.in66.com/api/payment/signature',
+  payUrl: 'https://love.in66.com/api/payment/signature',
   // businessParty: 'incrowdfunding_sh',
   businessParty: _station === '02' ? 'incrowdfunding_sh' : 'incrowdfunding',
   paymentChannel: 'weapppay', // wechatpay weapppay
